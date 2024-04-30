3ceb4a417539a67d12162047eb9eef516077cfb4
Mateus Dias <100047036+teus-levi@users.noreply.github.com>
2024-04-30 01:04:20 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>O bem que eu quero, esse eu não faço, o mau que não quero persegue os meus passos.</h1>
+    return <h1>O bem que eu quero, esse eu não faço, o mau que não quero persegue os meus passos. G3.</h1>
 }
 
 export default Home;
